64d288a5445beb4dcb68c624345278a94a8f4fb9
chan <cham9994@gmail.com>
2022-10-31 20:42:38 +0900

    feat: 이미지 추가

--- App.js
+++ App.js
@@ -11,11 +11,16 @@ export default function App() {
     setModalIsVisible(true)
   }
 
+  function endAddGoalHandler() {
+    setModalIsVisible(false)
+  }
+
   function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       { text: enteredGoalText, id: Math.random().toString() }
-    ])
+    ]),
+      endAddGoalHandler()
   }
 
   function deleteGoalHandler(id) {
@@ -27,7 +32,7 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <Button title="Add New Goal" color="#5e0acc" onPress={startAddGoalHandler} />
-      {modalIsVisible && <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} />}
+      {modalIsVisible && <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={endAddGoalHandler} />}
       <View style={styles.goalsContainer}>
         <FlatList
           data={courseGoals}
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, TextInput, View, Button, Modal } from 'react-native'
+import { StyleSheet, TextInput, View, Button, Modal, Image } from 'react-native'
 
 const GoalInput = (props) => {
   const [enteredGoalText, setEnteredGoalText] = useState('')
@@ -16,8 +16,16 @@ const GoalInput = (props) => {
   return (
     <Modal visible={props.visible} animationType="slie">
       <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require('../assets/images/goal.png')} />
         <TextInput style={styles.textInput} placeholder="Your course goal!" onChangeText={goalInputHandler} />
-        <Button onPress={addGoalHandler} title="Add Goal" />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button onPress={addGoalHandler} title="Add Goal" />
+          </View>
+          <View style={styles.button}>
+            <Button onPress={props.onCancel} title="Cancel"></Button>
+          </View>
+        </View>
       </View>
     </Modal>
   )
@@ -28,12 +36,18 @@ export default GoalInput
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 24,
+    padding: 16,
     borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
+    borderBottomColor: '#cccccc',
+    backgroundColor: '#311b6b'
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20
   },
   textInput: {
     borderWidth: 1,
@@ -41,5 +55,13 @@ const styles = StyleSheet.create({
     width: '70%',
     marginRight: 8,
     padding: 8
+  },
+  buttonContainer: {
+    margintTop: 16,
+    flexDirection: 'row'
+  },
+  button: {
+    width: '40%',
+    marginHorizontal: 8
   }
 })
